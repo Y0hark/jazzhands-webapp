9608bb3c2f35853aa3658316d91489003fd22e00
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-10-18 17:40:28 +0200

    [FIX]: trying type module config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const colors = require('vuetify/es5/util/colors').default
+import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
