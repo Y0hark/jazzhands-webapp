c211a5e4619914d4581209ca62340ba1c6e0aff9
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-11-22 00:35:09 +0100

    [ADD]: expansion panel to explain the ranking system

--- pages/rankings.vue
+++ pages/rankings.vue
@@ -1,6 +1,46 @@
 <template>
   <v-container grid-list-xs>
-    <h2 class="text-h5 text-center">this is the ranking page</h2>
+    <h2 class="text-h5 text-center mb-4">this is the ranking page</h2>
+    <v-expansion-panels v-model="panel">
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          How to improve my score?
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          The score is calculated with very simple rules. It takes in
+          consideration several things from your achievements:
+          <ul>
+            <li>
+              Your Miracle scores: your best performance (displayed on you
+              profile)
+            </li>
+            <li>Your Desolated Lands scores (also on your profile)</li>
+            <li>Your best Point War rank (on your player card)</li>
+            <li>Your best score in Sentinel Hunt</li>
+          </ul>
+          <br />
+          <i>You can improve them by doing these:</i>
+          <ul>
+            <li>For Miracles: go for higher tier or faster run</li>
+            <li>
+              For Desolated Lands: no need to speed things up, improving your
+              score should be the only focus
+            </li>
+            <li>For Point War: easy right? just aim for higher rank!</li>
+            <li>
+              For Sentinel Hunt: improve your score as we don't take the time
+              you take into consideration.
+            </li>
+          </ul>
+          <br />
+          Need help for this? Anytime you can ask the team to help you out!
+          We'll be happy yo provide help and advice.<br />
+
+          <b>Important note:</b> this is not a race. This is simply a number to
+          help you out tracking you own progress.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
     <br />
     <v-simple-table dense>
       <template v-slot:default>
@@ -28,7 +68,9 @@ import Ranker from "../services/rankingCalculation";
 export default {
   name: "rankingsPage",
   data() {
-    return {};
+    return {
+      panel: [0],
+    };
   },
   computed: {
     rankings() {
