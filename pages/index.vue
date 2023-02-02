76cda1126a7bf2d51ce477f969757a2192751b87
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2023-02-02 01:40:02 +0100

    [HOTFIX]: logo size on PC was too big

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 			<img
 				src="../assets/images/logo.jpg"
 				alt="logo"
-				class="mx-auto d-block w-50 h-50 mb-6"
+				class="mx-auto d-block mb-6 logo"
 			/>
 			<v-card :loading="bossMessageLoading" class="mb-2">
 				<v-card-title class="text-h6 text-mainText">
@@ -86,4 +86,9 @@ export default {
 	},
 };
 </script>
-<style></style>
+<style>
+.logo {
+	width: 200px;
+	height: 200px;
+}
+</style>
